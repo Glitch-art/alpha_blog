9686a32cba52d9e9b3a965c50e155b58b27b5bd3
andres <andres.carranza@sistemasaberes.com>
2022-06-30 14:08:06 +0000

    bootstrap repair

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '../js/bootstrap_js_files.js'  
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -13,3 +11,5 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "bootstrap"
